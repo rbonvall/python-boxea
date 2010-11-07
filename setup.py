bee787625fb3f8df59c6df0dd44b155f877ea6b7
Roberto Bonvallet <rbonvall@gmail.com>
2010-11-07 03:23:57 -0300

    Create minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='boxea',
+      version='0.0',
+      description='ASCII-art to Unicode box-drawing characters converter',
+      py_modules=['boxea'],
+      author='Roberto Bonvallet',
+      author_email='rbonvall@gmail.com',
+      url='https://github.com/rbonvall/python-boxea',
+)
